2b72dfde47882a70e94a5cd55a2fdbe18f0a8a19
Jack Day <jack94day@gmail.com>
2025-05-30 16:15:58 +0100

    Change the die to objects and add nanoid to generate a random key for the dice elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import Die from "./components/Die";
 import React from "react";
+import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = React.useState(generateAllNewDice);
 
   function generateAllNewDice() {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return new Array(10)
+      .fill(0)
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false, 
+        id: nanoid()
+      }));
   }
 
   function rollDice() {
     setDice(generateAllNewDice);
   }
 
-  const diceElements = dice.map((num) => <Die value={num} />);
+  const diceElements = dice.map(dieObj => <Die key={dieObj.id} value={dieObj.value} />);
 
   return (
     <main>
