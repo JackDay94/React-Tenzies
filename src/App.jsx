cbf87e8a794370889505f1a37eaced57d8c4df96
Jack Day <jack94day@gmail.com>
2025-05-30 17:10:44 +0100

    Add gameWon to track whether the game is won and update the roll dice button to new game

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,54 @@ import { nanoid } from "nanoid";
 function App() {
   const [dice, setDice] = React.useState(generateAllNewDice);
 
+  const gameWon = (
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value)
+  )
+
   function generateAllNewDice() {
-    return new Array(10)
-      .fill(0)
-      .map(() => ({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false, 
-        id: nanoid()
-      }));
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
   }
 
   function rollDice() {
-    setDice(prevDice => prevDice.map(dice => 
-      dice.isHeld === false ? {...dice, value: Math.ceil(Math.random() * 6)} : dice
-    ));
+    setDice((prevDice) =>
+      prevDice.map((dice) =>
+        dice.isHeld === false
+          ? { ...dice, value: Math.ceil(Math.random() * 6) }
+          : dice
+      )
+    );
   }
 
   function hold(id) {
-    setDice(prevDice => prevDice.map(dice =>
-      dice.id === id ? {...dice, isHeld: !dice.isHeld} : dice
-    ));
+    setDice((prevDice) =>
+      prevDice.map((dice) =>
+        dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice
+      )
+    );
   }
 
-  const diceElements = dice.map(dieObj => (
-    <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} hold={hold} id={dieObj.id} />)
-  );
+  const diceElements = dice.map((dieObj) => (
+    <Die
+      key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={hold}
+      id={dieObj.id}
+    />
+  ));
 
   return (
     <main>
-      <div className="dice-container">
-        {diceElements}
-      </div>
+      <div className="dice-container">{diceElements}</div>
 
-      <button className="roll-dice" onClick={rollDice}>Roll Dice</button>
+      <button className="roll-dice" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll Dice"}
+      </button>
     </main>
   );
 }
